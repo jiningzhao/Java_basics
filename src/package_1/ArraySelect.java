37b342192700c56929dcd26214af674fc90b32aa
赵吉宁 <zhaojining@Francis.local>
2019-12-10 00:09:12 +0800

    Java基础学习

--- src/package_1/ArraySelect.java
+++ src/package_1/ArraySelect.java
@@ -11,7 +11,11 @@ public class ArraySelect {
         int index = getIndex(arr,5);
         System.out.println("index="+index);
         int index_1 = getIndex_2(arr,24);
-        System.out.println(index_1);
+        System.out.println("24在数组中的位置为："+index_1);
+        int index_2 = getIndex_3(arr,25);
+        System.out.println("25插入数组的位置为："+index_2);
+        int index_3 = Arrays.binarySearch(arr,6); // 如果存在返回具体角标位置，不存在返回的是 -插入点-1
+        System.out.println(index_3);
     }
     /*
     正常查找
@@ -47,5 +51,31 @@ public class ArraySelect {
             }
         }
         return -1;
+    }
+    /*
+    面试题：
+    给定一个有序的数组，如果往该数组中存储一个元素，并保证这个数组还是有序的，
+    那么这个元素的存储的角标位如何获取。
+     */
+    public static int getIndex_3(int[] arr,int key){
+        Arrays.sort(arr);
+        ArraySort.printArray(arr);
+        int mid = arr.length/2;
+        int min = 0;
+        int max = arr.length-1;
+        while (min <= max){
+            if(arr[mid] == key){
+                return mid;
+            }
+            else if(arr[mid]>key){
+                max = mid-1;
+                mid = (max+min)/2;
+            }
+            else if(arr[mid]<key){
+                min = mid+1;
+                mid = (max+min)/2;
+            }
+        }
+        return min;
     }
 }
---
+++ src/package_1/ArrayTest1.java
@@ -0,0 +1,70 @@
+package package_1;
+/*
+获取一个整数的16禁止表现形式。
+ */
+public class ArrayTest1 {
+    public static void main(String[] args) {
+        toHex(60);
+        System.out.println();
+        toHex_1(60);
+        System.out.println();
+        toHex_2(60);
+        System.out.println();
+    }
+    /*
+    什么时候使用数组呢？
+    如果数据出现了对应关系，而且对应关系的一方是有序的数字编号。并作为角标使用。
+    这是就必须要想到数组的使用。
+
+    就可以将这些数据存储到数组中。
+    根据运算的结果作为角标直接去查数组中对应的元素即可。
+
+    这种方式称为：查表法。
+     */
+    public static void toHex_2(int num){
+        if(num == 0){
+            System.out.println("0");
+            return;
+        }
+        //定义一个对应关系表。
+        char[] chs = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','F'};
+        /*
+        一会查表会查到比较多的数据。
+        数据一多，就先存储起来，在进行操作。
+        所以定义一个数组。临时容器。
+         */
+        char[] arr = new char[8];
+        int pos = arr.length;
+        while(num != 0){
+            int temp = num & 15;
+            arr[--pos] = chs[temp];
+            num = num >>> 4;
+        }
+        System.out.println("pos="+pos);
+        for(int i = pos; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
+
+    }
+    public static void toHex_1(int num){
+
+        char[] chs = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','F'};
+
+        for(int x = 0; x < 8; x++){
+            int temp = num & 15;
+            System.out.print(chs[temp]);
+            num = num >>> 4;
+        }
+    }
+
+    public static void toHex(int num){
+        for(int x = 0; x < 8; x++){
+            int temp = num & 15;
+            if(temp > 9)
+                System.out.print((char)(temp-10+'A'));
+            else
+                System.out.print(temp);
+            num = num >>> 4;
+        }
+    }
+}
